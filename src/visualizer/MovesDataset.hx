c447bf817782e06cd79cd48bffb8231c6baa5a44
Christopher Foo <chris.foo@gmail.com>
2017-03-01 18:13:52 -0500

    Add and use MovesMap

--- src/visualizer/MovesDataset.hx
+++ src/visualizer/MovesDataset.hx
@@ -1,8 +1,10 @@
 package visualizer;
 
 
+typedef MovesMap = Map<String,MoveStats>;
+
 class MovesDataset extends Dataset {
-    public var moves(default, null):Dynamic;
+    public var moves(default, null):MovesMap;
 
     public function new() {
         super();
@@ -13,12 +15,17 @@ class MovesDataset extends Dataset {
     }
 
     override function loadDone(data:Dynamic) {
-        moves = data;
+        moves = new MovesMap();
+
+        for (slug in Reflect.fields(data)) {
+            moves.set(slug, MoveStats.fromJson(slug, Reflect.field(data, slug)));
+        }
+
         super.loadDone(data);
     }
 
     public function getMoveStats(slug:String, ?pokemonStat:PokemonStats):MoveStats {
-        var moveStat = MoveStats.fromJson(slug, Reflect.field(moves, slug));
+        var moveStat = moves.get(slug).clone();
 
         if (pokemonStat != null && slug == "hidden-power" && pokemonStat.moveTypeOverride != null) {
             moveStat.moveType = pokemonStat.moveTypeOverride;
